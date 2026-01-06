196ac4db528c10be7d74a34b0290acfe0e846a8b
ZZMolotov <zzmolotov8559@gmail.com>
2026-01-06 17:06:45 +0700

    lab13_1 fixed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,45 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double x[], int y, double z[]){
+    double sum = 0.;
+    double GM = 1;
+    double HMsum = 0;
+    double max = x[0];
+    double min = x[0];
+    double SD = 0;
+    for(int i = 0; i< y;i++ ){
+        sum+= x[i];
+    }
+    z[0] = sum/y;
+
+    for(int i = 0; i<y; i++){
+        SD += pow(x[i]-z[0],2);
+    }
+    z[1] = sqrt(SD/y);
+
+    for(int j = 0; j < y; j++){
+        GM *= x[j];
+    }
+    z[2] = pow(GM,1.0/y);
+
+    for(int k = 0; k < y; k++){
+        HMsum += 1/(x[k]);
+    }
+    z[3] = y/HMsum;
+
+    for(int h = 1; h< y;h++){
+        if(x[h] > max){
+            max = x[h];
+        }
+    }
+    z[4] = max;
+
+    for(int h = 1; h< y;h++){
+        if(x[h] < min){
+            min = x[h];
+        }
+    }
+    z[5] = min;
+}
